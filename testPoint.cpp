4866b3709a454011743483155282a72f1c6fe25b
sk2k0 <sk2k0@MSI>
2023-11-02 11:54:59 +0900

    プロジェクト ファイルを追加します。

---
+++ testPoint.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cstdio>
+using namespace std;
+
+void Average(int* student, int people)
+{
+	double total = 0;
+
+	for (int i = 0; i < people; ++i)
+	{
+		cout << i + 1 << "人目を入力してください > " << flush;
+		cin >> *(student + i);
+
+		total += *(student + i);
+	}
+	
+	printf("平均点は%.1f点です。", total / people);
+}
+
+int main()
+{
+	int people;
+	cout << "何名入力しますか？ > " << flush;
+	cin >> people;
+
+	int* student = new int[people];
+	
+	Average(student, people);
+		
+}
